c433d6d4caa2367f15b4c90dae8590e00b11e068
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-08-23 16:23:30 +0100

    broken

--- cucumber.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    default: {
-      require: ["src/steps/**/*.ts"],
-      requireModule: ["ts-node/register"], 
-      format: ["progress", "json:reports/cucumber-report.json"],
-      publishQuiet: true,
-      paths: ["src/features/**/*.feature"],
-    },
-  };
